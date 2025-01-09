17836dbc338ea6afa69eb67f1db9286655eae343
David Amsellem <davidamsellemquiros@gmail.com>
2025-01-09 12:48:06 +0100

    Modificaciones en el menu y responsibe

---
+++ src/routes/homePage/homePage.jsx
@@ -0,0 +1,21 @@
+import './homePage.scss'
+
+function HomePage() {
+return (
+    <div className='homePage'>
+        <div className="textContainer">
+            <div className="wrapper">
+                <h1>
+                La mayor red de colaboracion inmobiliarua a
+                 tu servicio
+                </h1>
+            </div>
+        </div>
+        <div className="imgContainer">
+            <img src="/bg.png" alt="fondobg" />
+        </div>
+    </div>
+)
+}
+
+export default HomePage
